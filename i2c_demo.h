d496cccb5f3c8986fadd843b9538cfa89e67aed4
René Rodríguez Paredes <zubh@protonmail.com>
2025-03-12 13:19:10 +0000

    Added READ/WRITE defines to the header

--- i2c_demo.h
+++ i2c_demo.h
@@ -1,13 +1,16 @@
 #ifdef EMULATOR
 #else
-  #include <neorv32.h>
+#include <neorv32.h>
 #endif
 
-#include <string.h>
 #include <stdint.h>
+#include <string.h>
 
 #define ACK 0
 #define NACK 1
+
+#define READ 1
+#define WRITE 0
 
 void print_hex_byte(uint8_t data);
 uint32_t aht20_begin(void);
